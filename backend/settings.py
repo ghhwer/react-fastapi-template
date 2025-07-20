c722721ba1c6bef7a5bfde677b754d00cece8b29
Caio Cominato <caiopetrellicominato@gmail.com>
2025-07-20 18:46:57 -0300

    feat: logging optional

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ API_ROUTE_PREFIX = os.environ.get("API_ROUTE_PREFIX", "/api/v1")
 DEBUG = os.environ.get("DEBUG", "false").lower() != "false"
 
 LOGSTASH_ENABLED = os.environ.get("LOGSTASH_ENABLED", "false").lower() != "false"
-LOG_DESTINATION = os.getenv("LOG_DESTINATION", "logstash:5044")
+LOG_DESTINATION = os.environ.get("LOG_DESTINATION", "logstash:5044")
 LOGSTASH_HOST, LOGSTASH_PORT = LOG_DESTINATION.split(":")
 LOGSTASH_PORT = int(LOGSTASH_PORT)
+LOG_TO_FILE = os.environ.get("LOG_TO_FILE", "false").lower() != "false"
